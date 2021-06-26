2340aa1841317a5c915994f9c538db62bd347f01
xayyam88 <xalid-7@mail.ru>
2021-06-26 18:39:35 +0400

    kanvas2

--- server.js
+++ server.js
@@ -5,28 +5,20 @@ const morgan = require('morgan');
 
 const path = require('path');
 
-const normalizePort = (port) => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-const dev = app.get('env') !== 'production';
 
-if (!dev) {
-    app.disable('x-powered-by');
-    app.use(compression());
-    app.use(morgan('common'));
+app.disable('x-powered-by');
+app.use(compression());
+app.use(morgan('common'));
 
-    app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-}
-
-if (dev) {
-    app.use(morgan('dev'));
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 const server = createServer(app);
 
